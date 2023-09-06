62a20f5c16607b6d578c16917c2d0c1a5cf6b200
Michael Valladolid <mikevalladolid@gmail.com>
2023-09-06 19:43:10 +0900

    PerpetualCache with tests

--- GenericCache.hpp
+++ GenericCache.hpp
@@ -1,9 +1,12 @@
 template <typename K, typename V>
 class GenericCache
 {
-    public:
-        virtual void set(K key, V value) = 0;
-        virtual V get(K key) = 0;
-        virtual void remove(K key) = 0;
-        virtual void clear() = 0;
+public:
+    virtual void set(const K &key, const V &value) = 0;
+
+    virtual V &get(const K &key) = 0;
+
+    virtual void remove(const K &key) = 0;
+
+    virtual void clear() = 0;
 };
